3bb37e942656b25ee6d0f3bec00d9bb498e61b9a
Olivier <olivier.tinh@gmail.com>
2018-11-14 16:45:09 -0500

    Timer is saved when the activity goes through a lifecycle

--- app/src/main/java/com/example/louisbertin/pomodoro/Tab1.java
+++ app/src/main/java/com/example/louisbertin/pomodoro/Tab1.java
@@ -1,10 +1,9 @@
 package com.example.louisbertin.pomodoro;
 
-import android.content.Context;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,18 +12,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TextView;
 
-import static android.content.Context.*;
-import static android.support.v4.content.ContextCompat.getSystemService;
-import static android.support.v4.content.ContextCompat.getSystemServiceName;
+public class Tab1 extends Fragment {
 
-public class Tab1 extends Fragment{
+    private final int ONE_SECOND = 1000;
 
     private Button bTimer;
     private CountDownTimer timer;
     private boolean running;
-    private long timeToCount=1500000;
+    private long timeToCount = 1500000;
     private Switch soundSwitch;
 
 
@@ -33,30 +29,58 @@ public class Tab1 extends Fragment{
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1, container, false);
 
-        bTimer=(Button) rootView.findViewById(R.id.timer_button);
-        bTimer.setOnClickListener(timeClick);
-        updateTimer((int)timeToCount/1000);
-        running=false;
+        getSavedInstance(savedInstanceState);
 
-        soundSwitch=(Switch)rootView.findViewById(R.id.silentSwitch);
-        soundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) ((MainActivity)getActivity()).setSoundOff();
-                else ((MainActivity)getActivity()).setSoundOn();
-                Log.d("pwt","value changed"+soundSwitch.isChecked());
-            }
-        });        return rootView;
+        setTimer(rootView);
+        setSoundSwitch(rootView);
+
+        if (running) startTimer();
+
+        return rootView;
     }
 
-    private View.OnClickListener timeClick=new View.OnClickListener() {
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong("time", timeToCount);
+        outState.putBoolean("running", running);
+    }
+
+    private void getSavedInstance(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            timeToCount = savedInstanceState.getLong("time");
+            running = savedInstanceState.getBoolean("running");
+        }
+    }
+
+    private View.OnClickListener timeClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (!running){
-                startTimer();
-            }else stopTimer();
+            if (!running) startTimer();
+            else stopTimer();
         }
     };
+
+    public void setTimer(View rootView) {
+        timer = new CountDownTimer(timeToCount, ONE_SECOND) {
+
+            @Override
+            public void onTick(long l) {
+                timeToCount -= ONE_SECOND;
+                updateTimer((int) l / ONE_SECOND);
+            }
+
+            @Override
+            public void onFinish() {
+                MediaPlayer mediaPlayer = MediaPlayer.create(getContext(), R.raw.airhorn);
+                mediaPlayer.start();
+                bTimer.setText("0:00");
+            }
+        };
+
+        bTimer = rootView.findViewById(R.id.timer_button);
+        bTimer.setOnClickListener(timeClick);
+        updateTimer((int) timeToCount / ONE_SECOND);
+    }
 
     public void updateTimer(int secondsLeft) {
         int minutes = secondsLeft / 60;
@@ -64,36 +88,34 @@ public class Tab1 extends Fragment{
 
         String secondString = Integer.toString(seconds);
 
-        if (seconds <10) {
+        if (seconds < 10) {
             secondString = "0" + secondString;
         }
 
-        bTimer.setText(Integer.toString(minutes) + ":" + secondString);
+        bTimer.setText(String.format("%s:%s", Integer.toString(minutes), secondString));
     }
 
-    public void startTimer(){
-            running = true;
-
-            timer = new CountDownTimer(timeToCount, 1000) {
-
-                @Override
-                public void onTick(long l) {
-                    updateTimer((int) l / 1000);
-                }
-
-                @Override
-                public void onFinish() {
-                    MediaPlayer mediaPlayer =MediaPlayer.create(getContext(),R.raw.airhorn);
-                    mediaPlayer.start();
-                    bTimer.setText("0:00");
-                }
-            }.start();
+    public void startTimer() {
+        running = true;
+        timer.start();
     }
 
-    public void stopTimer(){
+    public void stopTimer() {
+        running = false;
         timer.cancel();
-        running=false;
-        updateTimer((int)timeToCount/1000);
+        updateTimer((int) timeToCount / ONE_SECOND);
+    }
+
+    private void setSoundSwitch(View rootView) {
+        soundSwitch = rootView.findViewById(R.id.silentSwitch);
+        soundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) ((MainActivity) getActivity()).setSoundOff();
+                else ((MainActivity) getActivity()).setSoundOn();
+                Log.d("pwt", "value changed" + soundSwitch.isChecked());
+            }
+        });
     }
 
     public void setTimeToCount(long timeToCount) {
